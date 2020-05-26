fa3464330a006299b1ecdb26c1d6edad4dc0e3e4
nankouu <405.koji@gmail.com>
2020-05-26 16:42:38 +0000

    a

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_05_24_050338) do
+ActiveRecord::Schema.define(version: 2020_05_26_163929) do
 
   create_table "admins", force: :cascade do |t|
     t.string "email", default: "", null: false
