9e796a14335334b7862f3e3bb576d09a2640b48f
nankouu <405.koji@gmail.com>
2020-05-28 09:43:52 +0000

    refile model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_05_28_050904) do
+ActiveRecord::Schema.define(version: 2020_05_28_094038) do
 
   create_table "admins", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -115,6 +115,14 @@ ActiveRecord::Schema.define(version: 2020_05_28_050904) do
     t.index ["taggable_type"], name: "index_taggings_on_taggable_type"
     t.index ["tagger_id", "tagger_type"], name: "index_taggings_on_tagger_id_and_tagger_type"
     t.index ["tagger_id"], name: "index_taggings_on_tagger_id"
+  end
+
+  create_table "tags", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+    t.integer "taggings_count", default: 0
+    t.index ["name"], name: "index_tags_on_name", unique: true
   end
 
   create_table "users", force: :cascade do |t|
